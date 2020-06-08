b8fb9f52559b29fce6dbc3b13dabb3dd0bd535df
Sara Amiry <saraamiry24@gmail.com>
2020-06-08 21:23:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   smallest_key = nil
     name_hash.each do |key, value|
      if smallest_value == 0 || value < smallest_value
-    lowest_value = value
+    smallest_value = value
     lowest_key = key
      end
     end
